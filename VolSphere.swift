50f557909f2a67cf5a91f2d6ba3f36b7401576b0
Remy Skelton <remy.skelton@stu.ocsb.ca>
2025-02-11 03:42:31 +0000

    Started swift

---
+++ VolSphere.swift
@@ -0,0 +1,28 @@
+//
+//  VolSphere.swift
+//
+//  Created by Remy Skelton
+//  Created on 2025-February-10
+//  Version 1.0
+//  Copyright (c) Remy Skelton. All rights reserved.
+//
+//  This program will calculate the volume of a sphere.
+
+// import foundation
+import Foundation
+
+// Ask user to enter the radius
+print("Please enter the radius of the sphere in cm:")
+
+// Get user input
+if let input = readLine(), let radius = Double(input) {
+    if radius < 0 {
+        print("The radius must be positive.")
+    } else {
+        // Calculate the volume of the sphere
+        let volume = (4.0 / 3.0) * Double.pi * pow(radius, 3)
+        
+        // Display the volume
+        print("The volume of a sphere with a radius of " + "\(radius)" + " cm is " + String(format: "%.2f", volume) + " cm³")
+        }
+}
